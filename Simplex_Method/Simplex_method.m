6d3ddfd4076efd87cafaffc534a1ce9b9c3f8594
Amoiensis <yxp189@portonmail.com>
2019-10-20 21:08:47 +0800

    update Simplex_Method.

--- Simplex_Method/Simplex_method.m
+++ Simplex_Method/Simplex_method.m
@@ -1,8 +1,6 @@
-function [max_value,result,PRINT] = Simplex_method_f(A,b,C)
-% Simplex Method 单纯形法求解标准线性规划模型
-%   传入变量A、b、C 求解最优决策/最优值/最终单纯形表
+% Simplex Method Code
 %%
-% Author: Amoiensis
+% Author: Xiping Yu
 % Data: 2019.10.01
 %%
 % Structure（Program Simplex_Method）%
@@ -10,6 +8,35 @@ function [max_value,result,PRINT] = Simplex_method_f(A,b,C)
 % s.t. AX = b
 %      X >= 0
 % Program to solve the liner-program atomatically
+%%
+clear;clc;
+% Import Data %
+%%
+%无穷多最优解 %
+A = [...
+    2	2	1	0	0;
+    4	0	0	1	0;
+    0	5	0	0	1;...
+    ];
+b = [12 16 15]';
+C = [2 3 0 0 0];
+
+% 无界解 %
+% A = [...
+%     4	0	1....
+%     ];
+% b = [16]';
+% C = [2 3 0];
+
+% 无解 %
+% A = [...
+%     1	1	1	1	0	0	0;
+%     -2	1	-1	0	-1	1	0;
+%     0	3	1	0	0	0	1;...
+%     ];
+% b = [4 1 9]';
+% % C = [-3	0	1	0	0	-inf -inf];
+% C = [0	0	0	0	0	-1 -1];
 %%
 % 数据基础操作 %
 Size_A = size(A);
@@ -35,11 +62,11 @@ PUTOUT_C = " <<Column>> x_1~x_m   //  deta";
     if Size_A(1) == 1
         Position(find(A == 1)) = 1;
     else
-        Position = ((sum(A) == 1)&(min(A) == 0));
+        Position = (sum(A) == 1);
     end
     index_I = zeros(1,Size_A(1));
     for i=1:Size_A(2)
-       if  Position(i) 
+       if  Position(i)
            [~,temp] = max(A(:,i));
            index_I(temp) = i;
        end
@@ -54,105 +81,46 @@ PUTOUT_C = " <<Column>> x_1~x_m   //  deta";
     for i=1:Size_A(2)
         deta(i) = C(i) - (Coef)*A(:,i);
     end
-    [temp_deta,Position_in] = max(deta);    %找到换入位置
+    [temp_deta,Position_in] = max(deta);
     
     %%%%%%%%%%%%%%
     %打印单纯形表%
-    PRINT = zeros(1+Size_A(1),3+Size_A(2));
-    PRINT(1,1:3+Size_A(2)) =  [nan(1,3),C];
-    PRINT(2:end,1) =  Coef';
-    PRINT(2:end,2) =  Base';
-    PRINT(2:end,3) =  Decsion;
-    PRINT(2:end,4:3+Size_A(2)) = A;
+    PRINT = [];
+    PRINT(:,1) =  Coef';
+    PRINT(:,2) =  Base';
+    PRINT(:,3) =  Decsion;
+    PRINT(:,4:3+Size_A(2)) = A;
     PRINT(end+1,4:3+Size_A(2)) = deta;
-    PRINT(end,1:3) = [nan nan nan];
+    PRINT(end,1:3) = [inf inf inf];
     disp(strcat("步骤：",num2str(Step)));
     disp(PUTOUT_C);
     disp(PUTOUT_R);
     disp(PRINT);
-    %%%%%%%%%%%%%%
     Step = Step+1;
-    
-   %%%%%%%%%%%%%%
-    %sita
-    sita_arry = Decsion./A(:,Position_in);
-    Size_sita_arry = size(sita_arry);
-    for i=1:Size_sita_arry(1)
-        if sita_arry(i)<=0
-            sita_arry(i) = nan;
-        end
-    end
-    % 这里需要注意，先挑选出人工变量；
-    [sita,Position_out] = min(sita_arry);
-    
-    for i=1:Size_sita_arry(1)
-        if sita_arry(i) == sita
-               Base(Position_out) < Base(i);
-               Position_out = i;
-        end
-    end
-%     Position_out = Size_sita_arry(1)+1-Position_out;
-%     temp_num = A(Position_out,Position_in);
     %%%%%%%%%%%%%%
     
     %%%%%%%%%%%%%%
     %程序结束条件%
     
     %死循环-无界解跳出%
-%     @@这里的无界条件存在问题
-%     if A(:,Position_in) == ZEROS_ceofA
-%         FLAG = 2;
-%         break;
-%     end
-    
-%     if sum(deta>0) == Size_A(2)
-    %无界解
-%         for i=1:Size_A(2)
-%             if  deta(i)>0 && isequal(A(:,i),ZEROS_ceofA)
-%                 FLAG = 2;
-%                 break;
-%             end
-%         end
-%         if FLAG == 2
-%             break;
-%         end
-        if sita == inf
-            FLAG = 2;
-            break;
-        end
-%     end
+    if A(:,Position_in) == ZEROS_ceofA
+        FLAG = 2;
+        break;
+    end
     
     %正常求解-唯一解or无穷多解%
     if isequal(deta<=0,END_deta)
         FLAG = 1;
         break;
     end
-    
-    %无解
-%     for i=1:Size_A(2)
-%         if  deta(i)==0 && (sum(A(:,i)==1)~=1||sum(A(:,i)==0)~=(Size_A(1)-1))
-%             FLAG = 3;
-%             break;            
-%         end
-%     end
-%        if FLAG == 3
-%             break;
-%         end
-    
     %%%%%%%%%%%%%%
-%     %%%%%%%%%%%%%%
-%     %sita
-%     sita_arry = Decsion./A(:,Position_in);
-%     Size_sita_arry = size(sita_arry);
-%     for i=1:Size_sita_arry(1)
-%         if sita_arry(i)<=0
-%             sita_arry(i) = inf;
-%         end
-%     end
-%     [sita,Position_out] = min(sita_arry);
-%     
+    %%%%%%%%%%%%%%
+    %sita
+    sita_arry = Decsion./A(:,Position_in);
+    [sita,Position_out] = min(sita_arry);
+    
     temp_num = A(Position_out,Position_in);
-%     %%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%
 
     %行初等变换
     Decsion(Position_out) = Decsion(Position_out)/temp_num;
@@ -172,15 +140,11 @@ Result(Base) = Coef;
 if FLAG == 1
     temp_tell = deta == 0;
     for i=1:Size_A(2)
-        if deta(i)==0 && sum((sita>0))
-             FLAG = 12;   %无穷多
-             break;
-        end
-%        if  temp_tell(i)
-%            if sum((A(:,i)>0))
-%                FLAG = 12;   %无穷多
-%            end
-%        end
+       if  temp_tell(i)
+           if sum((A(:,i)>0))
+               FLAG = 12;
+           end
+       end
     end
     if FLAG ==12
         disp("该线性规划问题，存在“无穷多”最优解.");
@@ -193,21 +157,11 @@ if FLAG == 1
         disp("最优解决策为：(x1~xn)");
         disp(Result);
         disp("最优目标函数值为：");
-        disp(Coef*Decsion);
+        disp(Decsion*(Coef'));
     end
    
 else
     if FLAG == 2
         disp("该线性规划问题，其解无界（无限大）.");
-    else
-        if FLAG == 3
-            disp("该线性规划问题，无可行解.");
-        end
     end
 end
-%%
-% 结果返回 %
-result = Result;
-max_value = Coef*Decsion;
-end
-
